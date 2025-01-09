00029b57e5657f009ca34a1ce22045842005411e
DanielSurf10 <daniel101surf@gmail.com>
2025-01-09 19:22:45 -0300

    ex 01 do modulo 01 - Moar brainz!

--- Module_01/ex01/Zombie.cpp
+++ Module_01/ex01/Zombie.cpp
@@ -1,5 +1,9 @@
 #include "Zombie.hpp"
 
+Zombie::Zombie()
+{
+}
+
 Zombie::Zombie(std::string name) : _name(name)
 {
 	std::cout << this->_name << " is alive!" << std::endl;
@@ -13,4 +17,9 @@ Zombie::~Zombie()
 void	Zombie::announce(void)
 {
 	std::cout << this->_name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
+
+void	Zombie::set_name(std::string name)
+{
+	this->_name = name;
 }
