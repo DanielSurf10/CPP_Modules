3d0db4546ed5d7ec3af9c4a5ece8de02c166f387
DanielSurf10 <71985055+DanielSurf10@users.noreply.github.com>
2025-01-29 21:44:20 -0300

    testes do ex03 do modulo 03

--- Module_03/ex03/main.cpp
+++ Module_03/ex03/main.cpp
@@ -9,56 +9,55 @@ int main(void) {
 		DiamondTrap	blep("anormal");
 
 		std::cout << "Messages tests" << std::endl;
-		std::cout << blep.get_name() << std::endl;
-// 		blep.attack("You");
-// 		blep.attack("");
-// 		blep.takeDamage(5);
-// 		blep.beRepaired(2);
-//
-// 		std::cout << std::endl;
-// 		std::cout << "Health Points tests" << std::endl;
-//
-// 		blep.takeDamage(1);
-// 		blep.beRepaired(2);
-// 		blep.takeDamage(11);
-// 		blep.takeDamage(1);
-//
-// 		std::cout << std::endl;
-// 		std::cout << "Energy tests" << std::endl;
-//
-// 		/* setuping for energy tests */
-// 		blep.set_energy_points(0);
-// 		blep.set_hit_points(10);
-//
-// 		blep.attack("You again");
-// 		blep.beRepaired(1);
-// 		blep.attack("A ghost");
-// 		blep.guardGate();
-// 		blep.highFivesGuys();
+		blep.attack("You");
+		blep.attack("");
+		blep.takeDamage(5);
+		blep.beRepaired(2);
+
+		std::cout << std::endl;
+		std::cout << "Health Points tests" << std::endl;
+
+		blep.takeDamage(1);
+		blep.beRepaired(2);
+		blep.takeDamage(11);
+		blep.takeDamage(1);
+
+		std::cout << std::endl;
+		std::cout << "Energy tests" << std::endl;
+
+		/* setuping for energy tests */
+		blep.set_energy_points(0);
+		blep.set_hit_points(10);
+
+		blep.attack("You again");
+		blep.beRepaired(1);
+		blep.attack("A ghost");
+		blep.guardGate();
+		blep.highFivesGuys();
 		blep.whoAmI();
 	}
-// 	std::cout << std::endl;
-// 	std::cout << "Tests done, now is time to fight." << std::endl;
-// 	std::cout << std::endl;
-// 	{
-// 		ScavTrap	fighterOne("Guile");
-// 		FragTrap	fighterTwo("Tom");
-// 		DiamondTrap	fighterThree("Maguila");
-//
-// 		fighterOne.attack("Tom");
-// 		fighterTwo.takeDamage(fighterOne.get_attack_damage());
-// 		fighterTwo.attack("Maguila");
-// 		fighterThree.takeDamage(fighterTwo.get_attack_damage());
-// 		fighterThree.attack("Tom");
-// 		fighterOne.takeDamage(fighterThree.get_attack_damage());
-// 		fighterOne.guardGate();
-// 		fighterTwo.highFivesGuys();
-// 		fighterThree.whoAmI();
-// 		std::cout << "The fight ends cuz one fighter are in defensive mode "
-// 							<< "and the other guy are trying to make peace with a high five! "
-// 								 "And fighter three don't know who he are."
-// 							<< std::endl;
-// 	}
+	std::cout << std::endl;
+	std::cout << "Tests done, now is time to fight." << std::endl;
+	std::cout << std::endl;
+	{
+		ScavTrap	fighterOne("Guile");
+		FragTrap	fighterTwo("Tom");
+		DiamondTrap	fighterThree("Maguila");
+
+		fighterOne.attack("Tom");
+		fighterTwo.takeDamage(fighterOne.get_attack_damage());
+		fighterTwo.attack("Maguila");
+		fighterThree.takeDamage(fighterTwo.get_attack_damage());
+		fighterThree.attack("Tom");
+		fighterOne.takeDamage(fighterThree.get_attack_damage());
+		fighterOne.guardGate();
+		fighterTwo.highFivesGuys();
+		fighterThree.whoAmI();
+		std::cout << "The fight ends cuz one fighter are in defensive mode "
+							<< "and the other guy are trying to make peace with a high five! "
+								 "And fighter three don't know who he are."
+							<< std::endl;
+	}
 
 	return (0);
 }
