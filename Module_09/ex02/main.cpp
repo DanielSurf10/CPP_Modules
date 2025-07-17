6a7ccfb40d57b3857cf7a832b11ee05354d2d8c7
DanielSurf10 <71985055+DanielSurf10@users.noreply.github.com>
2025-07-16 23:06:22 -0300

    Revert "ex 02 do módulo 09 - PmergeMe"
    This reverts commit 14cb9b16f550a2c598156829d0d5e1d3c3ca61a5.

--- Module_09/ex02/main.cpp
+++ Module_09/ex02/main.cpp
@@ -4,62 +4,70 @@
 #include <iterator>
 #include "PmergeMe.hpp"
 
-int main(int argc, char **argv) {
+int main() {
+	// Example usage of PmergeMe
+	std::vector<int> vec;
+	vec.push_back(7);
+	vec.push_back(3);
+	vec.push_back(5);
+	vec.push_back(1);
+	vec.push_back(9);
+	vec.push_back(2);
+	vec.push_back(6);
+	vec.push_back(4);
+	vec.push_back(8);
+	vec.push_back(14);
+	vec.push_back(13);
+	vec.push_back(12);
+	vec.push_back(11);
+	vec.push_back(10);
+	std::vector<int>::iterator begin = vec.begin();
+	std::vector<int>::iterator end = vec.end();
+	PmergeMe<std::vector<int> >::sort(begin, end, 1);
 
-	if (argc < 2) {
-		std::cerr << "Testing with various vector sizes." << std::endl;
-
-		std::deque<int> vec;
-
-		for (int i = 1; i < 11000; i++) {
-			static int seed = 0;
-			srand(time(NULL) + seed++);
-			for (int j = 0; j < i; j++) {
-				vec.push_back(rand() % 10000);  // Random numbers between 0 and 99
-			}
-			PmergeMe::sort_deque(vec.begin(), vec.end(), 1);
-			// std::cout << i << " Sorted deque: ";
-			// std::copy(vec.begin(), vec.end(), std::ostream_iterator<int>(std::cout, " "));
-
-			bool isSorted = true;
-			for (std::deque<int>::iterator it = vec.begin(); it != vec.end() - 1; ++it) {
-				if (*it > *(it + 1)) {
-					isSorted = false;
-					break;
-				}
-			}
-			if (!isSorted) {
-				std::cout << "ERROR: deque is not sorted! " << i;
-				std::copy(vec.begin(), vec.end(), std::ostream_iterator<int>(std::cout, " "));
-				std::cout << std::endl;
-				return (0);
-			}
-
-			if (i % 100 == 0)
-				std::cout << i << std::endl;
-
-			vec.clear();
+	bool isSorted = true;
+	for (std::vector<int>::iterator it = vec.begin(); it != vec.end() - 1; ++it) {
+		if (*it > *(it + 1)) {
+			isSorted = false;
+			break;
 		}
 	}
-
-	try {
-		std::vector<int> vector_nums = PmergeMe::validateAndProcessInput<std::vector<int> >(argv, argc);
-		PmergeMe::displayTime_vector(vector_nums, "vector");
-	}
-	catch(const std::exception& e) {
-		std::cerr << "Error: " << e.what() << std::endl;
-		return (1);
+	if (!isSorted) {
+		std::cout << "ERROR: Vector is not sorted! ";
+		std::copy(vec.begin(), vec.end(), std::ostream_iterator<int>(std::cout, " "));
+		std::cout << std::endl;
+		return (0);
 	}
 
-	try {
-		std::deque<int> deque_nums = PmergeMe::validateAndProcessInput<std::deque<int> >(argv, argc);
-		PmergeMe::displayTime_deque(deque_nums, "deque");
-	}
-	catch(const std::exception& e) {
-		std::cerr << "Error: " << e.what() << std::endl;
-		return (1);
-	}
+	for (int i = 1; i < 11000; i++) {
+		static int seed = 0;
+		srand(time(NULL) + seed++);
+		for (int j = 0; j < i; j++) {
+			vec.push_back(rand() % 10000);  // Random numbers between 0 and 99
+		}
+		PmergeMe<std::vector<int> >::sort(vec.begin(), vec.end(), 1);
+		// std::cout << i << " Sorted vector: ";
+		// std::copy(vec.begin(), vec.end(), std::ostream_iterator<int>(std::cout, " "));
 
+		bool isSorted = true;
+		for (std::vector<int>::iterator it = vec.begin(); it != vec.end() - 1; ++it) {
+			if (*it > *(it + 1)) {
+				isSorted = false;
+				break;
+			}
+		}
+		if (!isSorted) {
+			std::cout << "ERROR: Vector is not sorted! " << i;
+			std::copy(vec.begin(), vec.end(), std::ostream_iterator<int>(std::cout, " "));
+			std::cout << std::endl;
+			return (0);
+		}
+
+		if (i % 100 == 0)
+			std::cout << i << std::endl;
+
+		vec.clear();
+	}
 
 	return 0;
 }
